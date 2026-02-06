fa533a2ba9e6249f6e446216d00ed33ba75f001b
rehan-shaikh14 <rehanalishaikh790@gmail.com>
2026-02-06 21:33:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
-
+# Coder: Rehan Shaikh
+# Date:30/1/26
+#class:F.E (COMPS) 
 print("--- Factorial Finder ---\n")
 
+n = int(input("Enter Number: "))
+factorial = 1
 
-# Write your code here
+if n >= 0:
+    for i in range(1, n + 1):
+        factorial = factorial * i
+    print(f"Factorial of {n} is {factorial}")
+else:
+    print(f"Factorial of {n} is Not Defined")
